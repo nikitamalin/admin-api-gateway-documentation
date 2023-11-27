c8976834f0a9fb78f329f1329e04e7cab2b1c4b1
nikitamalin <nikita@gocariq.com>
2023-11-27 15:09:38 -0800

    .:

--- pages/documentation.tsx
+++ pages/documentation.tsx
@@ -2,32 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useIframeContext } from "@/components/Context/iframe";
 import useSWR from "swr";
 import LoadingSpinner from "@/components/Auth/LoadingSpinner";
-/*
-export async function getStaticProps() &#123;
-  const path = require("path");
-  const fs = require("fs");
-  const filePath = path.join(process.cwd(), "/public/index.html");
-  const file = fs.readFileSync(filePath, "utf-8");
-  return &#123; props: &#123; file: file &#125; &#125;;
-&#125;*/
 
 export default function Doc() {
   let url = useIframeContext().url;
-  /*
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    // Code to run when the component is mounted
-    setMounted(true);
-    // Cleanup function (optional)
-    return () => {
-      // Code to run when the component is unmounted
-    };
-  }, []); // The empty dependency array means this effect will run once on mount and clean up on unmount
-
-  if (!mounted) {
-    return <LoadingSpinner />;
-  }*/
 
   const [loading, setLoading] = useState(true);
   const handleLoad = () => {
@@ -36,13 +13,13 @@ export default function Doc() {
   return (
     <div className="relative">
       {loading && (
-        <div className="absolute inset-0 flex items-center justify-center z-[50] ">
-          <LoadingSpinner />;
+        <div className="absolute inset-0 flex items-center justify-center h-[100vh] w-full bg-white z-[100]">
+          <LoadingSpinner />
         </div>
       )}
       <iframe
         src={url}
-        className="h-[100vh] w-full z-[1500]"
+        className={`h-[100vh] w-full ${loading ? "z-[-1]" : ""} `}
         onLoad={handleLoad}
       />
     </div>
