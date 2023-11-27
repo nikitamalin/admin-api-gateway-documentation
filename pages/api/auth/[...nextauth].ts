81409e3af394bb3a184095812341f2fb85a6d976
nikitamalin <nikita@gocariq.com>
2023-11-27 13:28:39 -0800

    .

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,51 @@
+import NextAuth from "next-auth";
+import { NextApiRequest, NextApiResponse } from "next";
+import Auth0Provider from "next-auth/providers/auth0";
+
+const {
+  AUTH0_CLIENT_ID = "",
+  AUTH0_CLIENT_SECRET = "",
+  AUTH0_ISSUER_BASE_URL = ""
+} = process.env;
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, {
+    providers: [
+      Auth0Provider({
+        clientId: AUTH0_CLIENT_ID,
+        clientSecret: AUTH0_CLIENT_SECRET,
+        issuer: AUTH0_ISSUER_BASE_URL
+      })
+    ],
+    callbacks: {
+      async signIn({ user, profile, account }) {
+        if (user && user.email) {
+          const whiteList = ["@gocariq.com"];
+          for (const domain of whiteList) {
+            if (user.email.endsWith(domain)) {
+              return true;
+            }
+          }
+
+          return true;
+        }
+        return "/not-authorized";
+      },
+      async session({ session, token, user }) {
+        session.idToken = token.idToken || "";
+        return session;
+      },
+      async jwt({ token, account }) {
+        if (account) {
+          token.idToken = account.id_token;
+        }
+        return token;
+      }
+    },
+    /*pages: {
+    //   error: '/auth/error',
+  },*/
+    debug: true,
+    secret: process.env.NEXTAUTH_SECRET
+  });
+}
