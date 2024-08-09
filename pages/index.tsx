eaad2a32cd962b85c81f5c527aa9408dbedd5bde
Nikita Malinovsky <nikita@cariqpay.com>
2024-08-08 19:44:25 -0700

    redoing home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,50 +67,12 @@ export default function Home() {
     <div className="flex flex-col max-w-[800px] w-[80vw] my-10 mx-auto ">
       <h1 className="mx-auto text-4xl">Car IQ Documentation</h1>
       <div className={`${subText} flex flex-col`}>
-        <h2 className="mt-10 text-xl">Get started with Insomnia!</h2>
-        <div className="ml-6 mt-2 flex flex-col">
-          <span>
-            1.&nbsp;
-            <Link
-              href="https://insomnia.rest/download"
-              target="_blank"
-              className="leading-6 underline cursor-pointer"
-            >
-              Download Insomnia
-            </Link>
-          </span>
-          <span>
-            2.&nbsp;
-            <button
-              onClick={handleDownload}
-              className="leading-6 underline cursor-pointer"
-            >
-              Download Car IQ API Dump File
-            </button>
-          </span>
-          <span>
-            3.&nbsp;
-            <Link
-              href="https://www.loom.com/share/5c588a379557438e964556131cc4d029"
-              target="_blank"
-              className="leading-6 underline cursor-pointer"
-            >
-              Watch Walkthrough Video
-            </Link>
-          </span>
-        </div>
-        <span className="mt-5 ">
-          Check out the&nbsp;
-          <Link href="/setup" className="leading-6 underline cursor-pointer">
-            setup docs
-          </Link>
-        </span>
         <span className="mt-5">
           Click any of the following queries/mutations to explore their fields
           and types.
         </span>
-        <h2 className={`${headingText}`}>Authentication</h2>
-        <div className={`${subHeading}`}>
+        <h3 className={`${headingText}`}>Authentication</h3>
+        <div className={`${subSubHeading}`}>
           <Link
             className={`${linkClass}`}
             href="/documentation"
@@ -118,443 +80,110 @@ export default function Home() {
           >
             userLogin
           </Link>
-          <span className={`${bulletSubSubHeading}`}>
-            &bull; gives you access token which can be used as a Bearer token
-            for requests.
-          </span>
         </div>
-        <h2 className={`${headingText}`}>Vehicle Fleet Management</h2>
-        <div className={`${subHeading}`}>
-          <h3 className={`${subHeadingText}`}>Single Vehicles</h3>
-          <div className={`${subSubHeading}`}>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("mutation-vehicleCreate")}
-            >
-              vehicleCreate
-            </Link>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("query-vehicle")}
-            >
-              vehicle
-            </Link>
-            <span className={`${bulletSubSubSubHeading}`}>
-              &bull; get a vehicle
-            </span>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("mutation-vehicleUpdate")}
-            >
-              vehicleUpdate
-            </Link>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("query-vehiclesFilter")}
-            >
-              vehiclesFilter
-            </Link>
-            <span className={`${bulletSubSubSubHeading}`}>
-              &bull; search across all vehicles in your organization
-            </span>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("query-vehiclesFilterByLocation")}
-            >
-              vehiclesFilterByLocation
-            </Link>
-          </div>
-          <h3 className={`${subHeadingText}`}>Vehicle Groups</h3>
-          <div className={`${subSubHeading}`}>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("mutation-vehicleGroupCreate")}
-            >
-              vehicleGroupCreate
-            </Link>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("query-vehicleGroup")}
-            >
-              vehicleGroup
-            </Link>
-            <span className={`${bulletSubSubSubHeading}`}>
-              &bull; get vehicle group
-            </span>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("mutation-vehicleGroupUpdate")}
-            >
-              vehicleGroupUpdate
-            </Link>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("mutation-vehicleGroupDelete")}
-            >
-              vehicleGroupDelete
-            </Link>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("query-vehicleGroupsFilter")}
-            >
-              vehicleGroupsFilter
-            </Link>
-            <span className={`${bulletSubSubSubHeading}`}>
-              &bull; filter on vehicle groups in your organization
-            </span>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() =>
-                setTheURL("mutation-vehicleGroupSpendRestrictionsSet")
-              }
-            >
-              vehicleGroupSpendRestrictionsSet
-            </Link>
-            <span className={`${bulletSubSubSubHeading}`}>
-              &bull; set restriction on spending such as amount per day, amount
-              per transactions, and at what times and days of week.
-            </span>
-          </div>
-          <h3 className={`${subHeadingText}`}>Vehicle Group Offline Mode</h3>
-          <div className={`${subSubHeading}`}>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("query-riskRuleGet")}
-            >
-              riskRuleGet
-            </Link>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("query-riskRulesList")}
-            >
-              riskRulesList
-            </Link>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("mutation-riskRuleCreate")}
-            >
-              riskRuleCreate
-            </Link>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("mutation-riskRuleUpdate")}
-            >
-              riskRuleUpdate
-            </Link>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("mutation-riskRuleDelete")}
-            >
-              riskRuleDelete
-            </Link>
-          </div>
-          <h3 className={`${subHeadingText}`}> Vehicle Group Drivers</h3>
-          <div className={`${subSubHeading}`}>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("mutation-vehicleGroupDriversAdd")}
-            >
-              vehicleGroupDriversAdd
-            </Link>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("mutation-vehicleGroupDriversRemove")}
-            >
-              vehicleGroupDriversRemove
-            </Link>
-          </div>
-          <h3 className={`${subHeadingText}`}> Vehicle Group Vehicles</h3>
-          <div className={`${subSubHeading}`}>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("mutation-vehicleGroupVehiclesAdd")}
-            >
-              vehicleGroupVehiclesAdd
-            </Link>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("mutation-vehicleGroupVehiclesRemove")}
-            >
-              vehicleGroupVehiclesRemove
-            </Link>
-            <Link
-              className={`${linkClass}`}
-              href="/documentation"
-              onClick={() => setTheURL("query-vehicleGroup")}
-            >
-              vehicleGroup
-            </Link>
-          </div>
-        </div>
-        <h2 className={`${headingText}`}>User Management</h2>
+
+        <h3 className={`${headingText}`}>Vehicle/Tolling</h3>
         <div className={`${subHeading}`}>
           <Link
             className={`${linkClass}`}
             href="/documentation"
-            onClick={() => setTheURL("mutation-userCreate")}
+            onClick={() => setTheURL("mutation-vehicleRegistrationDataUpdate")}
           >
-            userCreate
-          </Link>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("query-user")}
-          >
-            user
-          </Link>
-          <span className={`${bulletSubSubHeading}`}>&bull; get a user</span>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("mutation-userUpdate")}
-          >
-            userUpdate
-          </Link>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("mutation-userOffboard")}
-          >
-            userOffboard
+            vehicleRegistrationDataUpdate
           </Link>
           <span className={`${bulletSubSubHeading}`}>
-            &bull; offboard a user, will be deleted in 7 days.
+            &bull; Update Registration data on a vehicle which enables it for
+            tolling
           </span>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("query-usersFilter")}
-          >
-            usersFilter
-          </Link>
-          <span className={`${bulletSubSubHeading}`}>
-            &bull; filter on users in your entity
-          </span>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("mutation-userSetPassword")}
-          >
-            userSetPassword
-          </Link>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("mutation-userForgotPassword")}
-          >
-            userForgotPassword
-          </Link>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("mutation-userRefreshToken")}
-          >
-            userRefreshToken
-          </Link>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("mutation-userLoginOtpRequest")}
-          >
-            userLoginOtpRequest
-          </Link>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("mutation-userLoginOtpVerify")}
-          >
-            userLoginOtpVerify
-          </Link>
         </div>
-        <h2 className={`${headingText}`}>Pay App</h2>
+
+        <h3 className={`${headingText}`}>Service Location ETL</h3>
         <div className={`${subHeading}`}>
           <Link
             className={`${linkClass}`}
             href="/documentation"
-            onClick={() => setTheURL("mutation-servicePay")}
+            onClick={() => setTheURL("mutation-syncServiceLocationEnv")}
           >
-            servicePay
+            syncServiceLocationEnv
           </Link>
           <span className={`${bulletSubSubHeading}`}>
-            &bull; pay at a gas station with direct connect
+            &bull; Sync all stations from ETL to core_services for a given
+            environment
           </span>
           <Link
             className={`${linkClass}`}
             href="/documentation"
-            onClick={() => setTheURL("mutation-ttpTokenization")}
+            onClick={() => setTheURL("mutation-syncServiceLocations")}
           >
-            ttpTokenization
+            syncServiceLocations
           </Link>
           <span className={`${bulletSubSubHeading}`}>
-            &bull; provision a card for TTP
+            &bull; Sync a given list of stations from ETL to core_services
           </span>
           <Link
             className={`${linkClass}`}
             href="/documentation"
-            onClick={() => setTheURL("query-serviceLocationsFilter")}
+            onClick={() => setTheURL("mutation-updateStation")}
           >
-            serviceLocationsFilter
+            updateStation
           </Link>
           <span className={`${bulletSubSubHeading}`}>
-            &bull; get a list of stations near you
-          </span>
-        </div>
-        <h2 className={`${headingText}`}>Transactions</h2>
-        <div className={`${subHeading}`}>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("query-transactionsFilter")}
-          >
-            transactionsFilter
-          </Link>
-          <span className={`${bulletSubSubHeading}`}>
-            &bull; get a list of transactions
-          </span>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("query-transactionsStats")}
-          >
-            transactionsStats
-          </Link>
-          <span className={`${bulletSubSubHeading}`}>
-            &bull; get KPIs on all your transactions for a given entityId
+            &bull; Update data on a station
           </span>
         </div>
-        <h2 className={`${headingText}`}>Reporting/Invoicing</h2>
-        <div className={`${subHeading}`}>
+
+        <h3 className={`${headingText}`}>Card on File</h3>
+        <div className={`${subSubHeading}`}>
           <Link
             className={`${linkClass}`}
             href="/documentation"
-            onClick={() => setTheURL("query-transactionReportDownload")}
+            onClick={() => setTheURL("mutation-coFCardDelete")}
           >
-            transactionReportDownload
-          </Link>
-          <span className={`${bulletSubSubHeading}`}>
-            &bull; download transaction report on an entity, vehicle group,
-            vehicle, or a driver.
-          </span>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("query-reportsDownload")}
-          >
-            reportsDownload
-          </Link>
-          <span className={`${bulletSubSubHeading}`}>
-            &bull; download a transactions report on after hours purchases, all
-            transaction, transactions with premium fuel, validation exceptions
-            on transactions, or a purchase exception.
-          </span>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("query-driverReportDownload")}
-          >
-            driverReportDownload
-          </Link>
-          <span className={`${bulletSubSubHeading}`}>
-            &bull; download onboarded or offboarded drivers
-          </span>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("query-creditStatementsList")}
-          >
-            creditStatementsList
-          </Link>
-          <span className={`${bulletSubSubHeading}`}>
-            &bull; get a list of your credit statements
-          </span>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("query-creditPaymentsList")}
-          >
-            creditPaymentsList
-          </Link>
-          <span className={`${bulletSubSubHeading}`}>
-            &bull; get a list of payments for your credit account
-          </span>
-        </div>
-        <h2 className={`${headingText}`}>Policy Management</h2>
-        <div className={`${subHeading}`}>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("mutation-policyCreate")}
-          >
-            policyCreate
+            coFCardDelete
           </Link>
           <Link
             className={`${linkClass}`}
             href="/documentation"
-            onClick={() => setTheURL("mutation-policyManage")}
+            onClick={() => setTheURL("mutation-p97Tokenization")}
           >
-            policyManage
+            p97Tokenization
           </Link>
-          <span className={`${bulletSubSubHeading}`}>
-            &bull; update and enable/disable policies.
-          </span>
           <Link
             className={`${linkClass}`}
             href="/documentation"
-            onClick={() => setTheURL("mutation-policyDelete")}
+            onClick={() => setTheURL("mutation-vehiclePaymentMethodUpdate")}
           >
-            policyDelete
+            vehiclePaymentMethodUpdate
           </Link>
         </div>
-        <h2 className={`${headingText}`}>Entity</h2>
-        <div className={`${subHeading}`}>
+
+        <h3 className={`${headingText}`}>I2C</h3>
+        <div className={`${subSubHeading}`}>
           <Link
             className={`${linkClass}`}
             href="/documentation"
-            onClick={() => setTheURL("query-entity")}
+            onClick={() => setTheURL("mutation-accountCreditLimitSet")}
           >
-            entity
-          </Link>
-          <span className={`${bulletSubSubHeading}`}>
-            &bull; get information on an entity
-          </span>
-          <Link
-            className={`${linkClass}`}
-            href="/documentation"
-            onClick={() => setTheURL("query-entitiesFilter")}
-          >
-            entitiesFilter
+            accountCreditLimitSet
           </Link>
           <Link
             className={`${linkClass}`}
             href="/documentation"
-            onClick={() => setTheURL("query-entityAccountInfo")}
+            onClick={() => setTheURL("mutation-makePayment")}
           >
-            entityAccountInfo
+            makePayment
           </Link>
-          <span className={`${bulletSubSubHeading}`}>
-            &bull; information about the credit or ach account associated with
-            this entity
-          </span>
+        </div>
+
+        <h3 className={`${headingText}`}>Organization</h3>
+        <div className={`${subSubHeading}`}>
+          <Link
+            className={`${linkClass}`}
+            href="/documentation"
+            onClick={() => setTheURL("mutation-organizationUpdate")}
+          >
+            organizationUpdate
+          </Link>
         </div>
       </div>
     </div>
